65536e19a7b864785c81e3dec26be52aef637f93
Aldi Dharmawan <aldidharmawan700@gmail.com>
2024-10-31 09:56:15 +0700

    yes

--- resources/views/layouts/breadcrumb.blade.php
+++ resources/views/layouts/breadcrumb.blade.php
@@ -2,14 +2,14 @@
     <div class="card-body px-4 py-3">
         <div class="row align-items-center">
             <div class="col-9">
-                <h4 class="fw-semibold mb-8">{{ $title }}</h4>
+                <h4 class="fw-semibold mb-8 text-white">{{ $title }}</h4>
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb">
                         {{-- <li class="breadcrumb-item">
                             <a class="text-muted text-decoration-none"
                                 href="{{ route('dashboard') }}">{{ $subtitle }}</a>
                         </li> --}}
-                        <li class="breadcrumb-item" aria-current="page">{{ $title }}</li>
+                        <li class="breadcrumb-item text-white" aria-current="page">{{ $title }}</li>
                     </ol>
                 </nav>
             </div>
